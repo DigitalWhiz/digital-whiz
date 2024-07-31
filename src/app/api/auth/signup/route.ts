d6c6b3803c54e61c598ad8b13afb9e39ede9ce52
Agustina Acosta <agustinaa1503@gmail.com>
2024-07-31 16:40:19 -0300

    Digital Whiz Web

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,64 +1,64 @@
-// import { connectDB } from "@/libs/mongodb";
-// import User from "@/models/user";
-// import { NextResponse } from "next/server";
-// import bcrypt from "bcryptjs";
-// import mongoose from "mongoose";
+import { connectDB } from "@/libs/mongodb";
+import User from "@/models/user";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
-// export async function POST(request: Request) {
-//   try {
-//     await connectDB();
+export async function POST(request: Request) {
+  try {
+    await connectDB();
 
-//     const { name, email, password } = await request.json();
+    const { name, email, password } = await request.json();
 
-//     if (password < 6)
-//       return NextResponse.json(
-//         { message: "La contraseña debe tener al menos 6 caracteres" },
-//         { status: 400 }
-//       );
+    if (password < 6)
+      return NextResponse.json(
+        { message: "La contraseña debe tener al menos 6 caracteres" },
+        { status: 400 }
+      );
 
-//     const userFound = await User.findOne({ email });
+    const userFound = await User.findOne({ email });
 
-//     if (userFound)
-//       return NextResponse.json(
-//         {
-//           message: "El email ya existe, por favor inicia sesión",
-//         },
-//         {
-//           status: 409,
-//         }
-//       );
+    if (userFound)
+      return NextResponse.json(
+        {
+          message: "El email ya existe, por favor inicia sesión",
+        },
+        {
+          status: 409,
+        }
+      );
 
-//     const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-//     const user = new User({
-//       name,
-//       email,
-//       password: hashedPassword,
-//     });
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-//     const savedUser = await user.save();
-//     console.log(savedUser);
+    const savedUser = await user.save();
+    console.log(savedUser);
 
-//     return NextResponse.json(
-//       {
-//         name,
-//         email,
-//         createdAt: savedUser.createdAt,
-//         updatedAt: savedUser.updatedAt,
-//       },
-//       { status: 201 }
-//     );
-//   } catch (error) {
-//     if (error instanceof mongoose.Error.ValidationError) {
-//       return NextResponse.json(
-//         {
-//           message: error.message,
-//         },
-//         {
-//           status: 400,
-//         }
-//       );
-//     }
-//     return NextResponse.error();
-//   }
-// }
+    return NextResponse.json(
+      {
+        name,
+        email,
+        createdAt: savedUser.createdAt,
+        updatedAt: savedUser.updatedAt,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    return NextResponse.error();
+  }
+}
