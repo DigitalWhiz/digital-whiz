1a639531a9a1d8feecd74d95f6558ea5941d8808
Agustina Acosta <agustinaa1503@gmail.com>
2024-07-31 19:44:37 -0300

    Digital Whiz Web

--- src/components/section/sectionUno.tsx
+++ src/components/section/sectionUno.tsx
@@ -27,12 +27,12 @@ export default function SectionUno() {
 
         <div className="absolute top-0 left-12 p-8 sm:top-4 sm:left-4 sm:p-4">
           <h1
-            className="text-7xl sm:text-8xl font-extrabold text-[#C327E9] inline-block"
+            className="text-7xl sm:text-8xl font-extrabold text-[#C327E9] p-4 inline-block"
             style={{ textShadow: "15px 5px 5px rgba(0,0,0,0.8)" }}>
             DIGITAL <br /> WHIZ
           </h1>
           <p
-            className="text-white mt-4 text-4xl font-bold sm:text-lg sm:mt-2 md:text-3xl ml-4"
+            className="text-white mt-2 text-4xl font-bold sm:text-lg sm:mt-2 md:text-3xl ml-4"
             style={{ textShadow: "0 5px 5px rgba(128,0,128,0.8)" }}>
             Agencia <br />
             Marketing Integral
